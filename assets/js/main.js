368810474ffa0e87e85c1b1f550398c43ad03a5f
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-05-25 18:56:00 -0400

    Additional webpage, using buttons to select ingredients

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 
 	
+  var groupButtons = [
+    "Dairy", "Meat", "Fruits", "Vegetables", "Grains"
+  ];
+
+  var dairyButtons = [
+    "Milk", "Cheese", "Yogurt", "Sour Cream"
+  ];
+
+  for(i = 0; i < groupButtons.length; i++){
+      var groupButton = $('<button>').text(groupButtons[i]).attr('id', groupButtons[i]).addClass('groupButton');
+      $('#buttons').append(groupButton);
+    };
+
+  $('#buttons').on('click', '.groupButton', function() {
+    var foodGroup = $(this).attr("id");
+    if(foodGroup == "Dairy"){
+        for(i = 0; i < dairyButtons.length; i++){
+          var dairyButton = $('<button>').text(dairyButtons[i]).attr('id', dairyButtons[i]).addClass('dairyButton');
+          $('#buttons').append(dairyButton);
+        };
+    }
+  });
 
   $("div.blog-post").hover(
     function() {
